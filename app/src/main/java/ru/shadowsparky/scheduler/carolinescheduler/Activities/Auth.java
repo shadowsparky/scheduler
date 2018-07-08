f14ffd8b1acdcb4e52f8e7d6ee4ddb175dde76e1
Eugene <samsonov_eugene@mail.ru>
2018-07-08 22:04:08 +0300

    Авторизация завершена

--- app/src/main/java/ru/shadowsparky/scheduler/carolinescheduler/Activities/Auth.java
+++ app/src/main/java/ru/shadowsparky/scheduler/carolinescheduler/Activities/Auth.java
@@ -1,7 +1,13 @@
 package ru.shadowsparky.scheduler.carolinescheduler.Activities;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,42 +18,75 @@ import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.concurrent.TimeUnit;
+
 import ru.shadowsparky.scheduler.carolinescheduler.Exceptions.AuthDataNotFoundException;
 import ru.shadowsparky.scheduler.carolinescheduler.Interfaces.IAuthData;
 import ru.shadowsparky.scheduler.carolinescheduler.R;
 
-public class Auth extends AppCompatActivity implements View.OnClickListener, IAuthData {
-    static final int AUTH_DATA_SIZE = 2;
-    static final int LOGIN_INDEX = 0;
-    static final int PASSWORD_INDEX = 1;
-    static final String TAG = "MainTag";
+public class Auth extends AppCompatActivity implements View.OnClickListener, IAuthData{
+    public static final int AUTH_DATA_SIZE = 2;
+    public static final int LOGIN_INDEX = 0;
+    public static final int PASSWORD_INDEX = 1;
+    public static final String TAG = "MainTag";
+    public static final int REPETITIVE_AUTH = 1;
+    public static final int FIRST_AUTH = 0;
+    private AuthThread _thread;
+    Button AuthButton;
+    Button RegButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_auth);
-        Log.isLoggable(TAG, Log.DEBUG);
-        initListeners();
-        tryAuth();
+        if (!tryAuth()) {
+            setTheme(R.style.AppThemeWithStatusBarColor);
+            setContentView(R.layout.activity_auth);
+            Log.isLoggable(TAG, Log.DEBUG);
+            initListeners();
+        }
     }
 
     private void initListeners(){
-        Button AuthButton = (Button) findViewById(R.id.pushbutton);
-        Button RegButton = (Button) findViewById(R.id.regbutton);
+        AuthButton = (Button) findViewById(R.id.pushbutton);
+        RegButton = (Button) findViewById(R.id.regbutton);
         AuthButton.setOnClickListener(this);
         RegButton.setOnClickListener(this);
+    }
+
+    public void enableLoading(final ProgressDialog pd){
+        AuthButton.setEnabled(false);
+        pd.setTitle(getResources().getString(R.string.auth) + "...");
+        pd.setMessage(getResources().getString(R.string.AuthChecker));
+        pd.setButton(Dialog.BUTTON_NEGATIVE, "Отмена", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                _thread.cancel(true);
+            }
+        });
+        pd.show();
+    }
+
+    public void disableLoading(final ProgressDialog pd){
+        pd.hide();
+        AuthButton.setEnabled(true);
     }
 
     private boolean tryAuth(){
         String[] authData;
         try {
             authData = loadPreference();
-            auth(authData[LOGIN_INDEX], authData[PASSWORD_INDEX]);
+            auth(authData[LOGIN_INDEX], authData[PASSWORD_INDEX], REPETITIVE_AUTH);
         } catch (AuthDataNotFoundException e) {
             Log.d(TAG, e.getMessage());
             return false;
         }
         return true;
+    }
+
+    private void auth(String login, String password, int AuthControl){
+        String[] res = {login, password};
+        _thread = new AuthThread(this, AuthControl);
+        _thread.execute(res);
     }
 
     @Override
@@ -56,33 +95,15 @@ public class Auth extends AppCompatActivity implements View.OnClickListener, IAu
         String Password = ((TextView) findViewById(R.id.Password_ET)).getText().toString();
         switch (view.getId()){
             case R.id.pushbutton:
-                auth(Login, Password);
+                auth(Login, Password, FIRST_AUTH);
                 break;
             case R.id.regbutton:
-                Snackbar.make(view, getResources().getString(R.string.Auth_Failed), Snackbar.LENGTH_SHORT).show();
+                Snackbar.make(view, getResources().getString(R.string.Registration_Not_Available), Snackbar.LENGTH_SHORT).show();
                 break;
         }
     }
 
-    private void auth(String Login, String Password){
-        if ((Login.equals("test")) && (Password.equals("1111"))){
-            // ok
-            Boolean Check_Remember = ((CheckBox) findViewById(R.id.rememberbox)).isChecked();
-            if (Check_Remember){
-                try {
-                    savePreference(Login, Password);
-                } catch(InternalError e){
-                    Toast.makeText(getApplicationContext(), getResources().getString(R.string.Error_In_The_Save), Toast.LENGTH_SHORT).show();
-                    Log.d(TAG, e.getMessage());
-                }
-            }
-            goToMainActivity();
-        } else {
-            Log.d(TAG, "auth failed");
-            Snackbar.make(getCurrentFocus(), getResources().getString(R.string.Incorrect_login_or_password), Snackbar.LENGTH_SHORT).show();
-        }
-    }
-    private void goToMainActivity(){
+    public void goToMainActivity(){
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
         finish();
@@ -100,6 +121,14 @@ public class Auth extends AppCompatActivity implements View.OnClickListener, IAu
         }
     }
 
+    public boolean savePreferenceChecker(String Login, String Password){
+        boolean check = ((CheckBox) findViewById(R.id.rememberbox)).isChecked();
+        if (check) {
+            savePreference(Login, Password);
+        }
+        return check;
+    }
+
     @Override
     public String[] loadPreference() throws AuthDataNotFoundException {
         String result[] = new String[AUTH_DATA_SIZE];
@@ -110,5 +139,75 @@ public class Auth extends AppCompatActivity implements View.OnClickListener, IAu
             throw new AuthDataNotFoundException("Auth data not found");
         }
         return result;
+    }
+
+    public void raiseAuthError(){
+        Snackbar.make(getCurrentFocus(), getResources().getString(R.string.Incorrect_login_or_password), Snackbar.LENGTH_SHORT).show();
+    }
+}
+
+class AuthThread extends AsyncTask<String, Integer, String>{
+    public static final String USER_NOT_EXISTS = "user not Exists";
+    public static final String INTERRUPTED = "interrupted";
+    public static final String USER_EXISTS = "User Exists";
+    private Auth _auth;
+    private int _authControl;
+    private ProgressDialog pd ;
+
+    AuthThread(Auth auth, int AuthControl){
+        _auth = auth;
+        _authControl = AuthControl;
+        if (_authControl == Auth.FIRST_AUTH)
+            pd = new ProgressDialog(_auth);
+    }
+
+    @Override
+    protected void onPreExecute() {
+        if (_authControl == Auth.FIRST_AUTH)
+            _auth.enableLoading(pd);
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        if (_authControl == Auth.FIRST_AUTH)
+            _auth.disableLoading(pd);
+        switch (s){
+            case USER_EXISTS:
+                _auth.goToMainActivity();
+                break;
+            case USER_NOT_EXISTS:
+                _auth.raiseAuthError();
+                break;
+            case INTERRUPTED:
+                Log.d(Auth.TAG, INTERRUPTED);
+                break;
+        }
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        for (int i = 0; i < 5; i++){
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (isCancelled()) return INTERRUPTED;
+        }
+        if (fooAuth(strings[Auth.LOGIN_INDEX],strings[Auth.PASSWORD_INDEX])){
+            if (_authControl == Auth.FIRST_AUTH)
+                _auth.savePreferenceChecker(strings[Auth.LOGIN_INDEX],strings[Auth.PASSWORD_INDEX]);
+            return USER_EXISTS;
+        } else {
+            return USER_NOT_EXISTS;
+        }
+    }
+
+    private boolean fooAuth(String Login, String Password) {
+        if ((Login.equals("test") && (Password.equals("1111")))) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
