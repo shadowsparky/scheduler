6af4b962dbdc04821e0fce6a07cf327ca0656e05
Eugene <samsonov_eugene@mail.ru>
2018-07-13 20:57:52 +0300

    delete all fragments prepare to refactoring

--- app/src/main/java/ru/shadowsparky/scheduler/carolinescheduler/MVP/MainActivity/IMainContracts.java
+++ app/src/main/java/ru/shadowsparky/scheduler/carolinescheduler/MVP/MainActivity/IMainContracts.java
@@ -1,23 +1,9 @@
 package ru.shadowsparky.scheduler.carolinescheduler.MVP.MainActivity;
 
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import android.widget.ArrayAdapter;
-
-import java.util.ArrayList;
-
-import ru.shadowsparky.scheduler.carolinescheduler.Adapters.MainListAdapter;
-import ru.shadowsparky.scheduler.carolinescheduler.SQLite.Scheduler_Database;
-
 public interface IMainContracts {
-    interface MainFragmentContract{
-        void setAdapter(MainListAdapter adapter);
-    }
+
     interface MainViewContract{
         void setToolbar();
-        void itemClick(int position);
-        void itemFragmentLoad();
-        Context getContext();
     }
     interface MainPresenterContract{
 
--- app/src/main/java/ru/shadowsparky/scheduler/carolinescheduler/MVP/MainActivity/MainActivity.java
+++ app/src/main/java/ru/shadowsparky/scheduler/carolinescheduler/MVP/MainActivity/MainActivity.java
@@ -1,56 +1,19 @@
 package ru.shadowsparky.scheduler.carolinescheduler.MVP.MainActivity;
 
 import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.util.SortedList;
 import android.support.v7.widget.Toolbar;
-import android.util.AttributeSet;
-import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.Toast;
-
-import com.fasterxml.jackson.databind.JsonNode;
-
-import java.io.InputStream;
-import java.util.Map;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
-import butterknife.OnItemClick;
-import ru.shadowsparky.scheduler.carolinescheduler.Adapters.MainListAdapter;
-import ru.shadowsparky.scheduler.carolinescheduler.MVP.ChoosedItemFragment.ChoosedItem;
-import ru.shadowsparky.scheduler.carolinescheduler.MVP.NewScheduleFragment.NewSchedule;
 import ru.shadowsparky.scheduler.carolinescheduler.R;
-import ru.shadowsparky.scheduler.carolinescheduler.SQLite.Scheduler_Database;
 
 public class MainActivity extends AppCompatActivity implements IMainContracts.MainViewContract {
 
     @BindView(R.id.toolbar)
     Toolbar _toolbar;
-    private MainFragment _fragment;
     public Context getContext() {
         return getApplicationContext();
-    }
-    public void itemClick(int position) {
-        itemFragmentLoad();
-    }
-    public void itemFragmentLoad() {
-        Fragment ci = new ChoosedItem();
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out);
-        ft.replace(R.id.fragment_Container, ci);
-        ft.addToBackStack(null);
-        ft.commit();
-    }
-    public void setMainListAdapter(MainListAdapter adapter) {
-        _fragment.setAdapter(adapter);
     }
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,11 +22,6 @@ public class MainActivity extends AppCompatActivity implements IMainContracts.Ma
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         setToolbar();
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        _fragment = new MainFragment();
-        ft.replace(R.id.fragment_Container, _fragment);
-        ft.addToBackStack(null);
-        ft.commit();
     }
     public void setToolbar() {
         setSupportActionBar(_toolbar);
